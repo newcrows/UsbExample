3e237f5c4beb201e7fa93ad014627db59f0d270b
newcrows <34042296+newcrows@users.noreply.github.com>
2018-04-23 20:20:57 +0200

    Update AbstractPollThread.java

--- app/src/main/java/com/crowsnet/usbexample/AbstractPollThread.java
+++ app/src/main/java/com/crowsnet/usbexample/AbstractPollThread.java
@@ -1,87 +1,79 @@
-package com.crowsnet.usbexample;
-
-import android.support.annotation.NonNull;
-
-import java.io.IOException;
-import java.io.InputStream;
-
 public abstract class AbstractPollThread extends Thread {
 
-    //the default amount of bytes to read at once
-    public static final int DEFAULT_PACKET_SIZE = 4;
+//the default amount of bytes to read at once
+public static final int DEFAULT_PACKET_SIZE = 4;
 
-    //flag indicating wether this thread should still run
-    private volatile boolean isFinished = false;
+//flag indicating wether this thread should still run
+private volatile boolean isFinished = false;
 
-    //the input stream to obtain bytes from
-    private InputStream inputStream;
+//the input stream to obtain bytes from
+private InputStream inputStream;
 
-    //the packet size specified in constructor
-    private int packetSize;
+//the packet size specified in constructor
+private int packetSize;
 
-    //constructor using packetSize = DEFAULT_PACKET_SIZE
-    public AbstractPollThread(@NonNull InputStream inputStream) {
-        this(inputStream, DEFAULT_PACKET_SIZE);
-    }
+//constructor using packetSize = DEFAULT_PACKET_SIZE
+public AbstractPollThread(@NonNull InputStream inputStream) {
+    this(inputStream, DEFAULT_PACKET_SIZE);
+}
 
-    //constructor using custom packetSize
-    public AbstractPollThread(@NonNull InputStream inputStream, int packetSize) {
-        this.inputStream = inputStream;
-        this.packetSize = packetSize;
-    }
+//constructor using custom packetSize
+public AbstractPollThread(@NonNull InputStream inputStream, int packetSize) {
+    this.inputStream = inputStream;
+    this.packetSize = packetSize;
+}
 
-    //notify this Thread that it should finish
-    public void finish() {
-        isFinished = true;
-        interrupt();
-    }
+//notify this Thread that it should finish
+public void finish() {
+    isFinished = true;
+    interrupt();
+}
 
-    //this thread's loop method
-    @Override
-    public void run() {
-        //loop while finish() has not been called
-        while (!isFinished) {
-            try {
-                //obtain the next packetSize bytes as array
-                //this blocks until at least packetSize bytes are available
-                byte[] packet = nextPacket();
-
-                //invoke abstract handle method and pass the packet we just read
-                handlePacket(packet);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
+//this thread's loop method
+@Override
+public void run() {
+    //loop while finish() has not been called
+    while (!isFinished) {
         try {
-            //try to close the underlying stream
-            inputStream.close();
+            //obtain the next packetSize bytes as array
+            //this blocks until at least packetSize bytes are available
+            byte[] packet = nextPacket();
+
+            //invoke abstract handle method and pass the packet we just read
+            handlePacket(packet);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    //Child classes need to implement this method
-    protected abstract void handlePacket(byte[] packet);
-
-    private byte[] nextPacket() throws IOException {
-        //create a new byte array of packetSize length
-        byte[] bytePacket = new byte[packetSize];
-
-        //read in a loop, until exactly packetSize bytes are read
-        int c = 0;
-        while (c < packetSize) {
-            int r = inputStream.read(bytePacket, c, packetSize - c);
-            if (r == -1) {
-                //if this stream is closed, call finish() on this Thread
-                finish();
-            } else {
-                //increment the index by the amount of bytes we could read at once
-                c += r;
-            }
-        }
-
-        //return the bytePacket we just read
-        return bytePacket;
+    try {
+        //try to close the underlying stream
+        inputStream.close();
+    } catch (IOException e) {
+        e.printStackTrace();
     }
+}
+
+//Child classes need to implement this method
+protected abstract void handlePacket(byte[] packet);
+
+private byte[] nextPacket() throws IOException {
+    //create a new byte array of packetSize length
+    byte[] bytePacket = new byte[packetSize];
+
+    //read in a loop, until exactly packetSize bytes are read
+    int c = 0;
+    while (c < packetSize) {
+        int r = inputStream.read(bytePacket, c, packetSize - c);
+        if (r == -1) {
+            //if this stream is closed, call finish() on this Thread
+            finish();
+        } else {
+            //increment the index by the amount of bytes we could read at once
+            c += r;
+        }
+    }
+
+    //return the bytePacket we just read
+    return bytePacket;
 }
